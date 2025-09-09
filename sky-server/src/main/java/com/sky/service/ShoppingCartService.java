b69556fb5b4353e1d236b5ecfc68362425330ffe
SCUklr <3761297418@qq.com>
2025-09-09 17:22:51 +0800

    清空购物车和减少购物车内菜品数量的业务代码开发

--- sky-server/src/main/java/com/sky/service/ShoppingCartService.java
+++ sky-server/src/main/java/com/sky/service/ShoppingCartService.java
@@ -1,8 +1,26 @@
 package com.sky.service;
 
 import com.sky.dto.ShoppingCartDTO;
+import com.sky.entity.ShoppingCart;
+
+import java.util.List;
 
 public interface ShoppingCartService {
 
     void addShoppingCart(ShoppingCartDTO shoppingCartDTO);
+
+    /*
+    * 查看购物车
+    *
+    */
+
+    List<ShoppingCart> showShoppingCart();
+
+    /**
+     * 删除购物车中一个商品
+     * @param shoppingCartDTO
+     */
+    void subShoppingCart(ShoppingCartDTO shoppingCartDTO);
+
+    void cleanShoppingCart();
 }
